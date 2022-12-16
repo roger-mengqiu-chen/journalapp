11df501d2c6fef139cebd187fce393ada4067e28
Roger Chen <mengqiu.chen@hotmail.com>
2022-12-15 21:12:05 -0700

    Update

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    transaction_date = models.DateField(null=False, blank=False)
+    merchant = models.CharField(null=False, blank=False, max_length=255)
+    debit = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
+    credit = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
+    category = models.CharField(null=True, blank=True, max_length=255, default=None)
--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    date = models.DateField(null=False, blank=False)
+    name = models.CharField(null=False, blank=False, max_length=255)
+    description = models.TextField(null=True, blank=True, default=None)
+    location = models.CharField(null=True, blank=True, default=None)
+    category = models.CharField(null=False, blank=False, default="Life")
+
+
+class Person(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=255)
+    sex = models.CharField(null=False, blank=False, max_length=10)
+
+
+class PersonEvent(models.Model):
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+
