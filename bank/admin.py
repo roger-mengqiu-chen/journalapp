66300f704259a1caa4076901b3520a3a0d1b879d
Roger Chen <mengqiu.chen@hotmail.com>
2022-12-17 16:54:58 -0700

    Install django import export. Register admin sites

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from bank.models import Transaction, Category
+
 
 # Register your models here.
+class TransactionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Category, CategoryAdmin)
--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from journal.models import Event, Person
+
+
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Person, PersonAdmin)
