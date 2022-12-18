e153bbe529f538301d66f69c8b598660a8413eb1
Roger Chen <mengqiu.chen@hotmail.com>
2022-12-18 09:08:24 -0700

    Update display for transaction admin

--- bank/admin.py
+++ bank/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from bank.models import Transaction, Category
+from django.contrib.admin.views.main import ChangeList
 
 
 # Register your models here.
 class TransactionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    pass
+    list_display = ("get_transaction_date",
+                    "get_transaction_merchant",
+                    "get_transaction_debit",
+                    "get_transaction_credit",
+                    "get_transaction_category")
+    list_display_links = ("get_transaction_merchant",)
+
+
 
 
 class CategoryAdmin(admin.ModelAdmin):
