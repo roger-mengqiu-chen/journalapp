ab9e336a43ee2c7d6bb12b15c0e08f57fee032e5
Roger Chen <mengqiu.chen@hotmail.com>
2022-12-17 21:42:44 -0700

    Update admin to display more info

--- journal/admin.py
+++ journal/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 
-from journal.models import Event, Person
+from journal.models import Event, Person, Category
 
 
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_event_date', 'name', 'get_event_description', 'get_event_location', 'get_event_people')
 
 
 class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_person_sex')
+
+
+class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Category, CategoryAdmin)
