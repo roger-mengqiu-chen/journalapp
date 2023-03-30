d044e57a156b8eb0b61e5b4b850172d2a6942679
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2023-03-29 22:29:58 -0600

    Update Person model and created PersonEvent model

--- journal/models.py
+++ journal/models.py
@@ -8,7 +8,6 @@ class Event(models.Model):
     description = models.TextField(null=True, blank=True, default=None)
     location = models.CharField(null=True, blank=True, default=None, max_length=255)
     category = models.ForeignKey("Category", default=None, on_delete=models.PROTECT)
-    people = models.ManyToManyField("Person")
 
     def __str__(self):
         return self.name
@@ -27,7 +26,9 @@ class Event(models.Model):
 
     @admin.display(description="People")
     def get_event_people(self):
-        return ', '.join([a.name for a in self.people.all()])
+        person_ids = list(PersonEvent.objects.filter(event=self).values_list("person_id", flat=True))
+        people = list(Person.objects.filter(id__in=person_ids))
+        return people
 
     def get_event_category(self):
         return self.category
@@ -36,6 +37,8 @@ class Event(models.Model):
 class Person(models.Model):
     name = models.CharField(null=False, blank=False, max_length=255)
     sex = models.CharField(null=False, blank=False, max_length=10)
+    phone_number = models.CharField(null=True, blank=True, max_length=50)
+    address = models.CharField(null=True, blank=True, max_length=255)
 
     class Meta:
         verbose_name_plural = "People"
@@ -46,6 +49,11 @@ class Person(models.Model):
     @admin.display(description="Sex")
     def get_person_sex(self):
         return self.sex
+
+
+class PersonEvent(models.Model):
+    person = models.ForeignKey(Person, null=False, blank=False, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, null=False, blank=False, on_delete=models.CASCADE)
 
 
 class Category(models.Model):
