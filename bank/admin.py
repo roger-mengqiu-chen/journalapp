5bfe5d56f67637eb314ac96e6042b297e87e56c1
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2023-05-24 21:51:04 -0600

    Add button for transaction admin

--- bank/admin.py
+++ bank/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from import_export.admin import ImportExportModelAdmin
 from bank.models import Transaction, Category
-from django.contrib.admin.views.main import ChangeList
 
 
 class TransactionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
@@ -9,16 +9,21 @@ class TransactionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
                     "merchant",
                     "debit",
                     "credit",
-                    "category")
+                    "category",
+                    "action")
     list_display_links = ("merchant",)
     search_fields = ["merchant"]
+    autocomplete_fields = ["category"]
     list_per_page = 15
 
     list_editable = ["category"]
 
+    def action(self, obj):
+        return format_html('<a class="btn" href="/admin/bank/transaction/{}/delete/">Delete</a>', obj.id)
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["category"]
 
 
 admin.site.register(Transaction, TransactionAdmin)
