0bc0ac6382df38f3860aed726c153b2e46089ea5
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-10 13:11:20 -0600

    set up models for note

--- note/models.py
+++ note/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Note(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
