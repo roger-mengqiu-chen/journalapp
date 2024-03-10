2d922d879ca4dfd97883fb0e7fef207a5da8c799
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-10 12:53:47 -0600

    use mysql

--- journalapp/settings.py
+++ journalapp/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'journalapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'journalapp',
+        'USER': 'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
