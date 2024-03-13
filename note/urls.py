22991a69515c9fe01576231b98431e5514fbe483
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-12 20:41:12 -0600

    updated with drf serializer

--- note/urls.py
+++ note/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:page_number>', views.notes, name='notes'),
+    path('notes/<int:page_number>', views.notes, name='notes'),
     path('<int:note_id>', views.note, name='notes'),
 ]
 
