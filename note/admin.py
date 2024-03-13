653a2124d4f72fed0899130354dcb92715f11c5e
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2024-03-12 20:42:37 -0600

    Dev (#17)
    * updated requirement
    
    * create app note
    
    * set up models for note
    
    * add rest framework
    
    * add urls in note
    
    * add apis for note
    
    * use drf to setup apis
    
    * created admin for note
    
    * updated with drf serializer

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from note.models import Note, Tag, Category
+
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ["title"]
+    list_filter = ['created_at', 'updated_at']
+
+admin.site.register(Note, NoteAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ["name"]
+
+admin.site.register(Tag, TagAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ["name"]
+
+admin.site.register(Category, CategoryAdmin)
--- note/views.py
+++ note/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from note.models import Note
+from note.serializers import NoteSerializer
+
 
 @api_view(['GET', 'POST', 'DELETE', 'PUT'])
 def note(request, note_id):
-    return Response({'note': 'note'})
+    note_1 = Note.objects.get(note_id=note_id)
+    res = NoteSerializer(note_1).data
+    return Response(res)
 
 
 @api_view(['GET'])
