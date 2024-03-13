85606bbd8755ecdc894247339f26d1f01a0acb40
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2024-03-12 20:15:26 -0600

    Dev (#15)
    * updated requirement
    
    * create app note
    
    * set up models for note
    
    * add rest framework
    
    * add urls in note
    
    * add apis for note
    
    * use drf to setup apis

--- note/views.py
+++ note/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET', 'POST', 'DELETE', 'PUT'])
+def note(request, note_id):
+    return Response({'note': 'note'})
+
+
+@api_view(['GET'])
+def notes(request, page_number):
+    return Response({'notes': 'notes'})
