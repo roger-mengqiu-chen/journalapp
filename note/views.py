aca96dc83b893aa156d610e6dd0dbe49c1771580
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-15 20:46:13 -0600

    Update note function

--- note/views.py
+++ note/views.py
@@ -2,14 +2,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from note.models import Note
+from note.note_services import get_note_by_id, delete_note_by_id
 from note.serializers import NoteSerializer
 
 
-@api_view(['GET', 'POST', 'DELETE', 'PUT'])
+@api_view(['GET', 'DELETE', 'PUT'])
 def note(request, note_id):
-    note_1 = Note.objects.get(note_id=note_id)
-    res = NoteSerializer(note_1).data
-    return Response(res)
+
+    note = get_note_by_id(note_id)
+    if note is None:
+        return Response({'message': 'Note not found'}, status=404)
+    
+    if request.method == 'GET':
+        return Response(NoteSerializer(note).data)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response({'message': 'Note deleted'}, status=204)
 
 
 @api_view(['GET'])
