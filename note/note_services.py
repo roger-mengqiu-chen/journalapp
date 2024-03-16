24411f3340a71e5911219caa94397dcf93df929d
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-15 20:45:36 -0600

    remove delete_note_by_id

--- note/note_services.py
+++ note/note_services.py
@@ -7,14 +7,3 @@ def get_note_by_id(id):
         return note
     except Note.DoesNotExist:
         return None
-
-
-def delete_note_by_id(id):
-    try:
-        note = Note.objects.get(note_id=id)
-        note.delete()
-        return True
-    except Note.DoesNotExist:
-        return False
-
-
