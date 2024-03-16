cffc9c053737a7fd79ac5c9c26bd83665c17c072
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-15 20:31:53 -0600

    add serializers for tags and category

--- note/serializers.py
+++ note/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from note.models import Note
+from note.models import Note, Category, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class NoteSerializer(serializers.ModelSerializer):
+
+    tags = TagSerializer(many=True)
+    category = CategorySerializer()
+
     class Meta:
         model = Note
-        fields = '__all__'
+        fields =  ['note_id', 'title', 'content', 'created_at', 
+                   'updated_at', 'tags', 'category']
+    
